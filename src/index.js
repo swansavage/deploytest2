b92a2ed5e09b03af986d7e3d68304719f9d78468
swansavage@github.com <swansavage@gmail.com>
2023-08-17 17:22:35 -0500

    fixed again

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="portfoliotwo">
+    <BrowserRouter basename="deploytest2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
